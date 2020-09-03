02649339325b57a559e9a1424cae0617096433dd
felipebma <fbma@cin.ufpe.br>
2020-09-03 01:37:43 -0300

    Implementing User findById with exception handler

--- src/main/java/com/felipearaujo/forum/resources/UserResource.java
+++ src/main/java/com/felipearaujo/forum/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +20,19 @@ public class UserResource {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> users = userService.findAll();
 		List<UserDTO> usersDTO = users.stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
 		return ResponseEntity.ok(usersDTO);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = userService.findById(id);
+		UserDTO userDTO = new UserDTO(user);
+		return ResponseEntity.ok(userDTO);
 	}
 
 }
--- src/main/java/com/felipearaujo/forum/services/UserService.java
+++ src/main/java/com/felipearaujo/forum/services/UserService.java
@@ -1,12 +1,15 @@
 package com.felipearaujo.forum.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.felipearaujo.forum.domain.entities.User;
 import com.felipearaujo.forum.repositories.UserRepository;
+import com.felipearaujo.forum.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +20,14 @@ public class UserService {
 	public List<User> findAll() {
 		List<User> users = userRepository.findAll();
 		return users;
+	}
+	
+	public User findById(String id) {
+		try {
+			Optional<User> user = userRepository.findById(id);
+			return user.get();
+		}catch(NoSuchElementException e) {
+			throw new ObjectNotFoundException("Id not found : " + id);
+		}
 	}
 }
