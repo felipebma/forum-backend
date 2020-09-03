8e1c08b91f46501d67cf3152d0d2aae9811172a9
felipebma <fbma@cin.ufpe.br>
2020-09-02 23:09:43 -0300

    First Commit

---
+++ src/test/java/com/felipearaujo/forum/ForumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipearaujo.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
